f379ff1a33e0f138e5e2de7afce0a0aa7ef8bdf0
Dennis <111556958+ddpadilla@users.noreply.github.com>
2026-03-19 16:46:21 +0000

    deploy live pgos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   supabaseUrl: 'https://lpooruluvctjaqrexsee.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxwb29ydWx1dmN0amFxcmV4c2VlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1OTIzMDgsImV4cCI6MjA4OTE2ODMwOH0.GbT-TAS3t84kTd17hGHOZLcyWL07DdPSMgbfQGrWHsg',
-  paypalClientId: 'sb',
-  paypalSandbox: true,
+  paypalClientId: 'PAYPAL_CLIENT_ID',
+  paypalSandbox: false,
   exchangeRateHNLtoUSD: 0.041,
   // Datos bancarios para transferencias
   bankAccount: '2001112233',
