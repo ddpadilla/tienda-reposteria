65f01a9e8c5a94afffce1976193e5ac30a4c6d23
Dennis <111556958+ddpadilla@users.noreply.github.com>
2026-03-19 19:38:13 +0000

    Exchange rate

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxwb29ydWx1dmN0amFxcmV4c2VlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1OTIzMDgsImV4cCI6MjA4OTE2ODMwOH0.GbT-TAS3t84kTd17hGHOZLcyWL07DdPSMgbfQGrWHsg',
   paypalClientId: 'AXZZFUpdv3T0edRCjXs6LF9_KSzvHiiSOsPj404vErMxTvtudy4itxKC-ElRfkhPZ5z7j56tNDd4apK_',
   paypalSandbox: false,
-  exchangeRateHNLtoUSD: 0.041,
+  exchangeRateHNLtoUSD: 0.03604,
   // Datos bancarios para transferencias
   bankAccount: '2001112233',
   bankName: 'Banco de Honduras',
